9eac2197e4bbe4cb583efb5c13ce430034bd9454
Elektropay <richard.r@everpayinc.com>
2020-08-07 22:06:14 -0500

    Update Footer.js

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -25,6 +25,26 @@ class Footer extends Component {
             { title : "Login", link : "/page-cover-login" },
         ],
         grid2 : [
+            { title : "About us", link : "/page-aboutus" },
+            { title : "Services", link : "/page-services" },
+            { title : "Team", link : "/page-team" },
+            { title : "Pricing", link : "/page-pricing" },
+            { title : "Project", link : "/page-work" },
+            { title : "Careers", link : "/page-jobs" },
+            { title : "Blog", link : "/page-blog" },
+            { title : "Login", link : "/page-cover-login" },
+        ],
+        grid3 : [
+            { title : "About us", link : "/page-aboutus" },
+            { title : "Services", link : "/page-services" },
+            { title : "Team", link : "/page-team" },
+            { title : "Pricing", link : "/page-pricing" },
+            { title : "Project", link : "/page-work" },
+            { title : "Careers", link : "/page-jobs" },
+            { title : "Blog", link : "/page-blog" },
+            { title : "Login", link : "/page-cover-login" },
+        ],
+        grid4 : [
             { title : "Terms of Services", link : "/terms" },
             { title : "Privacy Policy", link : "/privacy-policy" },
             { title : "Documentation", link : "/documentation" },
@@ -43,18 +63,8 @@ class Footer extends Component {
 
             <Container>
                 <Row>
-                    <Col lg="4" xs="12" className="mb-0 mb-md-4 pb-0 pb-md-2" name="footercolumn">
-                        <Link className={this.props.isLight ? "logo-footer text-dark" : "logo-footer"} to="#">Elektropay<span className="text-primary">.</span></Link>
-                        <p className={ this.props.isLight ? "mt-4 text-muted" : "mt-4"}>Start working with Landrick that can provide everything you need to generate awareness, drive traffic, connect.</p>
-                        <ul className="list-unstyled social-icon social mb-0 mt-4">
-                            <li className="list-inline-item mr-1"><Link to="" className="rounded"><i className="mdi mdi-facebook" title="Facebook"></i></Link></li>
-                            <li className="list-inline-item mr-1"><Link to="" className="rounded"><i className="mdi mdi-instagram" title="Instagram"></i></Link></li>
-                            <li className="list-inline-item mr-1"><Link to="" className="rounded"><i className="mdi mdi-twitter" title="Twitter"></i></Link></li>
-                        </ul>
-                    </Col>
-                
                     <Col lg="2" md="4" xs="12" className="mt-4 mt-sm-0 pt-2 pt-sm-0" name="footercolumn">
-                        <h4 className={this.props.isLight ? "text-dark footer-head" : "text-light footer-head"}>Company</h4>
+                        <h4 className={this.props.isLight ? "text-dark footer-head" : "text-light footer-head"}>Platform</h4>
                         <ul className="list-unstyled footer-list mt-4">
                             {
                                 this.state.grid1.map((grid, key) =>
@@ -63,9 +73,8 @@ class Footer extends Component {
                             }
                         </ul>
                     </Col>
-                    
-                    <Col lg="3" md="4" xs="12" className="mt-4 mt-sm-0 pt-2 pt-sm-0" name="footercolumn">
-                        <h4 className={this.props.isLight ? "text-dark footer-head" : "text-light footer-head"}>Useful Links</h4>
+                    <Col lg="2" md="4" xs="12" className="mt-4 mt-sm-0 pt-2 pt-sm-0" name="footercolumn">
+                        <h4 className={this.props.isLight ? "text-dark footer-head" : "text-light footer-head"}>Features</h4>
                         <ul className="list-unstyled footer-list mt-4">
                             {
                                 this.state.grid2.map((grid, key) =>
@@ -74,24 +83,27 @@ class Footer extends Component {
                             }
                         </ul>
                     </Col>
-
+                
+                    <Col lg="2" md="4" xs="12" className="mt-4 mt-sm-0 pt-2 pt-sm-0" name="footercolumn">
+                        <h4 className={this.props.isLight ? "text-dark footer-head" : "text-light footer-head"}>Company</h4>
+                        <ul className="list-unstyled footer-list mt-4">
+                            {
+                                this.state.grid3.map((grid, key) =>
+                                <li key={key}><Link to={grid.link} className={this.props.isLight ? "text-muted" : "text-foot"}><i className="mdi mdi-chevron-right mr-1"></i> {grid.title}</Link></li>
+                                )
+                            }
+                        </ul>
+                    </Col>
+                    
                     <Col lg="3" md="4" xs="12" className="mt-4 mt-sm-0 pt-2 pt-sm-0" name="footercolumn">
-                        <h4 className={this.props.isLight ? "text-dark footer-head" : "text-light footer-head"}>Newsletter</h4>
-                        <p className="mt-4">Sign up and receive the latest tips via email.</p>
-                        <Form>
-                            <Row>
-                                <Col lg="12">
-                                    <div className={this.props.isLight ? "foot-subscribe foot-white form-group position-relative" : "foot-subscribe form-group position-relative"}>
-                                        <Label className={this.props.isLight ? "text-muted" : ""}>Write your email <span className="text-danger">*</span></Label>
-                                        <i className="mdi mdi-email ml-3 icons"></i>
-                                        <Input type="email" name="email" id="emailsubscribe" className={this.props.isLight ? "form-control bg-light border pl-5 rounded" : "form-control pl-5 rounded"} placeholder="Your email : " required/>
-                                    </div>
-                                </Col>
-                                <Col lg="12">
-                                    <Input type="submit" id="submitsubscribe" name="send" className="btn btn-primary rounded w-100" value="Subscribe"/>
-                                </Col>
-                            </Row>
-                        </Form>
+                        <h4 className={this.props.isLight ? "text-dark footer-head" : "text-light footer-head"}>Resources</h4>
+                        <ul className="list-unstyled footer-list mt-4">
+                            {
+                                this.state.grid4.map((grid, key) =>
+                                <li key={key}><Link to={grid.link} className={this.props.isLight ? "text-muted" : "text-foot"}><i className="mdi mdi-chevron-right mr-1"></i> {grid.title}</Link></li>
+                                )
+                            }
+                        </ul>
                     </Col>
                 </Row>
             </Container>
@@ -100,6 +112,11 @@ class Footer extends Component {
             <Container className="text-center">
                 <Row className="align-items-center">
                     <Col md="4" sm="6">
+                      <ul className="list-unstyled social-icon social mb-0 mt-4">
+                            <li className="list-inline-item mr-1"><Link to="" className="rounded"><i className="mdi mdi-facebook" title="Facebook"></i></Link></li>
+                            <li className="list-inline-item mr-1"><Link to="" className="rounded"><i className="mdi mdi-instagram" title="Instagram"></i></Link></li>
+                            <li className="list-inline-item mr-1"><Link to="" className="rounded"><i className="mdi mdi-twitter" title="Twitter"></i></Link></li>
+                        </ul>
                         <div className="text-sm-left small">
                             <p className="mb-0 small">Elektropay is a service of EverPay. &copy; 2020 EverPay.</p>
                         </div>
