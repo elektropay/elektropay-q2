d5f168ff8370cf51adcb85727640e77dfb116e8c
Elektropay <richard.r@everpayinc.com>
2020-08-09 05:27:42 -0500

    Update section.js

--- src/pages/Software/section.js
+++ src/pages/Software/section.js
@@ -15,7 +15,7 @@ class Section extends Component {
                         <Row className="align-items-center position-relative mt-5" style={{zIndex: "1"}}>
                             <Col lg="6" md="12">
                                 <div className="title-heading mt-4 text-center text-lg-left">
-                                    <h1 className="heading mb-3 text-dark text-hero">Monetize Your Commerce Channels</h1>
+                                    <h1 className="heading mb-3 text-dark text-hero">Monetize All Your Commerce Channels</h1>
                                     <p className="lead text-muted">Elektropay is a commerce as service management platform that delivers innovative payment solutions for start-ups and enterprise businesses alike.</p>
                                     <div className="mt-4">
                                          <Link to="" className="btn btn-light mt-1 mr-2 mouse-down"> Request A Demo</Link>
