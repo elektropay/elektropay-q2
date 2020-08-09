3765bde2872221c488267805f3f606060763a584
Elektropay <richard.r@everpayinc.com>
2020-08-09 04:55:16 -0500

    Update feature-description.js

--- src/pages/Software/feature-description.js
+++ src/pages/Software/feature-description.js
@@ -12,7 +12,7 @@ class FeatureDescription extends Component {
                 <Row className="align-items-center">
                     <Col lg="7">
                         <div className="saas-feature-shape-left position-relative">
-                            <img src={this.props.image1} className="img-fluid mx-auto d-block rounded shadow" alt=""/>
+                            <img src={this.props.app1} className="img-fluid mx-auto d-block rounded shadow" alt=""/>
                         </div>
                     </Col>
 
@@ -46,7 +46,7 @@ class FeatureDescription extends Component {
                     <Col lg={{size :5, order :1}} xs={{order:2}}  className="mt-4 pt-2 mt-lg-0 pt-lg-0">
                         <div className="section-title mr-lg-4">
                             <h1 className="title mb-3">Reach more customers in more places</h1>
-                            <p className="para-desc text-muted">Use our extensive global payments network and technologies to accesss the global market.</p>
+                            <p className="para-desc text-muted">Use our extensive global payments network and technologies to access global markets.</p>
                         
                             <Row>
                                 {
@@ -72,7 +72,7 @@ class FeatureDescription extends Component {
                     
                     <Col lg={{size:7, order:2}} xs={{order:1}}>
                         <div className="saas-feature-shape-right position-relative">
-                            <img src={this.props.image1} className="img-fluid mx-auto d-block rounded shadow" alt=""/>
+                            <img src={this.props.app2} className="img-fluid mx-auto d-block rounded shadow" alt=""/>
                         </div>
                     </Col>
                 </Row>
