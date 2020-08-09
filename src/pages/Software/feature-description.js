8f5a7ce0aebf900dbe4338c7384ad1d660599caf
Elektropay <richard.r@everpayinc.com>
2020-08-09 05:35:37 -0500

    Update feature-description.js

--- src/pages/Software/feature-description.js
+++ src/pages/Software/feature-description.js
@@ -8,43 +8,8 @@ class FeatureDescription extends Component {
     render() {
         return (
             <React.Fragment>
-            <Container className="pb-5 mb-md-5 mt-100 mt-60 mb-60">
-                <Row className="align-items-center">
-                    <Col lg="7">
-                        <div className="saas-feature-shape-left position-relative">
-                            <img src={this.props.image1} className="img-fluid mx-auto d-block rounded shadow" alt=""/>
-                        </div>
-                    </Col>
 
-                    <Col lg="5" className="mt-4 pt-2 mt-lg-0 pt-lg-0">
-                        <div className="section-title ml-lg-4">
-                            <h1 className="title mb-3">Optimize Sales Conversions</h1>
-                            <p className="para-desc text-muted">Launch your payments program quickly and benefit from our extensive expertise, deep relationships with financial institutions and leading technology companies.</p>
-                            
-                            <OwlCarousel
-                                className="owl-theme"
-                                items = {1}
-                                loop
-                                margin={10}
-                            >
-                                {
-                                    this.props.testimonials.map((testimonial, key) =>
-                                        <div key={key} className="customer-testi py-3">
-                                            <img src={testimonial.image} style={{height:65, width:65}} className="img-fluid avatar avatar-small rounded-pill shadow" alt=""/>
-                                            <p className="text-muted mt-4">" {testimonial.desc} "</p>
-                                            <h6 className="text-primary">- {testimonial.name}</h6>
-                                        </div>
-                                    )
-                                }
-                            </OwlCarousel>
-                        </div>
-                    </Col>
-                               <Col xs="12" className="pt-4">
-                                    <Link to="" className="btn btn-dark">Find out how</Link>
-                                </Col>
-                </Row>
-            </Container>
-            <Container className="mt-100 mt-60 mb-100">
+            <Container className="mt-100 mt-60 mb-5">
                 <Row className="align-items-center">
                     <Col lg={{size :5, order :1}} xs={{order:2}}  className="mt-4 pt-2 mt-lg-0 pt-lg-0">
                         <div className="section-title mr-lg-4">
@@ -77,6 +42,42 @@ class FeatureDescription extends Component {
                         <div className="saas-feature-shape-right position-relative">
                             <img src={this.props.image2} className="img-fluid mx-auto d-block rounded shadow" alt=""/>
                         </div>
+                    </Col>
+                </Row>
+            </Container>
+            <Container className="pb-5 mb-md-5 mt-100 mt-60 mb-5">
+                <Row className="align-items-center mb-5">
+                    <Col lg="7">
+                        <div className="saas-feature-shape-left position-relative">
+                            <img src={this.props.image1} className="img-fluid mx-auto d-block rounded shadow" alt=""/>
+                        </div>
+                    </Col>
+
+                    <Col lg="5" className="mt-4 pt-2 mt-lg-0 pt-lg-0">
+                        <div className="section-title ml-lg-4">
+                            <h1 className="title mb-3">Optimize Sales Conversions</h1>
+                            <p className="para-desc text-muted">Launch your payments program quickly and benefit from our extensive expertise, deep relationships with financial institutions and leading technology companies.</p>
+                            
+                            <OwlCarousel
+                                className="owl-theme"
+                                items = {1}
+                                loop
+                                margin={10}
+                            >
+                                {
+                                    this.props.testimonials.map((testimonial, key) =>
+                                        <div key={key} className="customer-testi py-3">
+                                            <img src={testimonial.image} style={{height:65, width:65}} className="img-fluid avatar avatar-small rounded-pill shadow" alt=""/>
+                                            <p className="text-muted mt-4">" {testimonial.desc} "</p>
+                                            <h6 className="text-primary">- {testimonial.name}</h6>
+                                        </div>
+                                    )
+                                }
+                            </OwlCarousel>
+                        </div>
+                              <Col xs="12" className="mt-4">
+                                    <Link to="" className="btn btn-dark">Find out how</Link>
+                                </Col>
                     </Col>
                 </Row>
             </Container>
