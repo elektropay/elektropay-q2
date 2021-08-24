30a962ed2580877e0672c63423f98d3b3c56e93f
Elektropay <richard.r@everpayinc.com>
2021-08-24 01:11:57 -0500

    Update PageContactUs.js
    Updated the mailing address

--- src/pages/Contact/PageContactUs.js
+++ src/pages/Contact/PageContactUs.js
@@ -118,7 +118,7 @@ class PageContactUs extends Component {
                                     </div>
                                     <div className="content mt-3">
                                         <h4 className="title font-weight-bold">Location</h4>
-                                        <p className="text-muted">7 Mornington Cresent, Manchester, H4K 4K8, UK<br />Houston, UK 485</p>
+                                        <p className="text-muted">2e Parkinson Rd, Ste 34296, Liverpool<br />2e Parkinson Rd, Ste 34296, Liverpool, England, L9 1DL, UK</p>
                                         <Link to="//www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin" className="video-play-icon h6 text-primary">View on Google map</Link>
                                     </div>
                                 </div>
