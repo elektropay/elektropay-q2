5c2590e3218f4fc475b4117490ebb4257acf7477
Elektropay <richard.r@everpayinc.com>
2021-10-03 11:49:48 -0500

    Update PageContactUs.js

--- src/pages/Contact/PageContactUs.js
+++ src/pages/Contact/PageContactUs.js
@@ -1,4 +1,4 @@
-// React Basic and Bootstrap
+E// React Basic and Bootstrap
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col, Alert, Form, FormGroup, Input, Label } from 'reactstrap';
@@ -8,7 +8,7 @@ import PageBreadcrumb from "../../../components/Shared/page-breadcrumb";
 
 // import images
 import bitcoin from '../../../images/icon/bitcoin.svg';
-import Email from '../../../images/icon/Email.svg';
+import email from '../../../images/icon/Email.svg';
 import location from '../../../images/icon/location.svg';
 import contact from '../../../images/contact.png';
 
@@ -118,7 +118,9 @@ class PageContactUs extends Component {
                                     </div>
                                     <div className="content mt-3">
                                         <h4 className="title font-weight-bold">Location</h4>
-                                        <p className="text-muted">2e Parkinson Rd, Ste 34296, Liverpool<br />2e Parkinson Rd, Ste 34296, Liverpool, England, L9 1DL, UK</p>
+    
+                                    <p className="text-muted"><br /> 2e Parkinson Rd, Ste 34296, Liverpool, England, L9 1DL
+</p>
                                         <Link to="//www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin" className="video-play-icon h6 text-primary">View on Google map</Link>
                                     </div>
                                 </div>
