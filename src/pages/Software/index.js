0c5019662360e7d2ee68fc0d006686510cecdcc5
Elektropay <richard.r@everpayinc.com>
2024-03-31 01:23:09 -0500

    Update index.js

--- src/pages/Software/index.js
+++ src/pages/Software/index.js
@@ -25,12 +25,12 @@ import client6 from "../../images/client/06.jpg";
 class IndexclassNameSoftware extends Component {
     state = {
         testimonials : [
-            { id : 1, name : "Thomas Israel", image : client1, desc : "It seems that only fragments of the original text remain in the Lorem Ipsum texts used today."},
-            { id : 2, name : "Carl Oliver", image : client2, desc : "The most well-known dummy text is the 'Lorem Ipsum', which is said to have originated in the 16th century."},
-            { id : 3, name : "Barbara McIntosh", image : client3, desc : "One disadvantage of Lorum Ipsum is that in Latin certain letters appear more frequently than others."},
-            { id : 4, name : "Jill Webb", image : client4, desc : "Thus, Lorem Ipsum has only limited suitability as a visual filler for German texts."},
-            { id : 5, name : "Dean Tolle", image : client5, desc : "There is now an abundance of readable dummy texts. These are usually used when a text is required."},
-            { id : 6, name : "Christa Smith", image : client6, desc : "According to most sources, Lorum Ipsum can be traced back to a text composed by Cicero."},
+            { id : 1, name : "Thomas Israel", image : client1, desc : "Their prompt responses and professional support is top-notch, even on a Sunday."},
+            { id : 2, name : "Dan Oliver", image : client2, desc : "With a wide range of options and competitive rates that make it possible for you to manage your payments more effectively."},
+            { id : 3, name : "Jill Stetnascewski", image : client3, desc : "We always recieved our payments on time and their platform simplifies the tracking of it all under one roof."},
+            { id : 4, name : "Barbara Webb", image : client4, desc : "Simply the best! Being a customer for over a year now, it seems that their customer service is always quick and round the clock."},
+            { id : 5, name : "Dean Tolle", image : client5, desc : "Embedding Elektropay into our products through their API has empowered us to sell to more markets"},
+            { id : 6, name : "Christa Smith", image : client6, desc : "Our customer pays in GBP that converts to USD that settles in our account in Hong Kong. Elektropay pricing was the best that found."},
         ],
         features : [
             { imgUrl : user, title : "Easy To Use", description : "Nisi aenean vulputate eleifend tellus vitae eleifend enim a Aliquam eleifend aenean elementum semper." },
